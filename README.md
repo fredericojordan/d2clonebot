c28c2184612b97f0de3ae94ef53dc5439bd6d439
Frederico Jordan <fredericojordan@gmail.com>
2022-04-26 20:23:59 -0300

    Proactive status reports

--- README.md
+++ README.md
@@ -1,1 +1,27 @@
-# D2 Diablo Clone
+# Diablo II: Resurrected - Diablo Clone Tracker Discord Bot
+
+[![powered-by-diablo2.io](https://img.shields.io/badge/powered%20by-diablo2.io-green)](https://diablo2.io/)
+
+This is a bot that scrapes the [Diablo Clone Tracker](https://diablo2.io/dclonetracker.php) API and offers some functionalities on Discord:
+
+1. `!uberdiablo` command for Diablo Clone tracker verification.
+2. Proactive reports when the tracker progresses.
+
+## Quickstart
+
+Click [this link](`https://discord.com/api/oauth2/authorize?client_id=968258121803915264&permissions=3072&scope=bot`) to add D2 Clone bot to your server!
+
+## Running the bot
+
+You may also download the code, alter it in any fashion and run the bot on your own computer.
+
+1. Create a [Discord Application](https://discord.com/developers/applications), enable a bot account and create its authentication token.
+2. Set your token as the `DISCORD_TOKEN` environment variable by running `export DISCORD_TOKEN=YOUR_TOKEN_HERE`.
+   1. Optionally set the `DISCORD_CHANNEL_ID` to match the ID of the channel you wish to receive proactive status updates.
+3. Download and run the bot by executing the following:
+   ```shell
+   git clone https://github.com/fredericojordan/d2clonebot.git
+   cd d2clonebot
+   python d2clone.py
+   ```
+4. Generate an invite link and add the bot to your Discord server.
--- d2clone.py
+++ d2clone.py
@@ -1,25 +1,30 @@
 import os
 
+import discord
 import requests
-from discord.ext import commands
+from discord.ext import tasks
 
 BASE_URL = "https://diablo2.io/dclone_api.php"
+DISCORD_CHANNEL_ID = int(os.environ.get("DISCORD_CHANNEL_ID", 0))
 
 
-class RegionFilter:
-    REGION_AMERICAS = 1
-    REGION_EUROPE = 2
-    REGION_ASIA = 3
+class Regions:
+    AMERICAS = 1
+    EUROPE = 2
+    ASIA = 3
+    TEXT = {1: "Americas", 2: "Europe", 3: "Asia"}
 
 
-class LadderFilter:
+class Ladder:
     LADDER = 1
     NON_LADDER = 2
+    TEXT = {1: "Ladder", 2: "Non-ladder"}
 
 
-class HardcoreFilter:
+class Hardcore:
     HARDCORE = 1
     SOFTCORE = 2
+    TEXT = {1: "Hardcore", 2: "Softcore"}
 
 
 class SortDirection:
@@ -46,30 +51,95 @@ def get_diablo_tracker(
         "sd": sort_direction,
     }
     filtered_params = {k: v for k, v in params.items() if v is not None}
-    return requests.get(BASE_URL, params=filtered_params)
+    headers = {"User-Agent": "d2clone"}
+    response = requests.get(BASE_URL, params=filtered_params, headers=headers)
+    return response.json() if response.status_code == 200 else None
 
 
-bot = commands.Bot(command_prefix="!")
+class DCloneTracker:
+    def __init__(self):
+        self.progress = {
+            (Regions.AMERICAS, Ladder.LADDER, Hardcore.HARDCORE): None,
+            (Regions.AMERICAS, Ladder.LADDER, Hardcore.SOFTCORE): None,
+            (Regions.AMERICAS, Ladder.NON_LADDER, Hardcore.HARDCORE): None,
+            (Regions.AMERICAS, Ladder.NON_LADDER, Hardcore.SOFTCORE): None,
+            (Regions.EUROPE, Ladder.LADDER, Hardcore.HARDCORE): None,
+            (Regions.EUROPE, Ladder.LADDER, Hardcore.SOFTCORE): None,
+            (Regions.EUROPE, Ladder.NON_LADDER, Hardcore.HARDCORE): None,
+            (Regions.EUROPE, Ladder.NON_LADDER, Hardcore.SOFTCORE): None,
+            (Regions.ASIA, Ladder.LADDER, Hardcore.HARDCORE): None,
+            (Regions.ASIA, Ladder.LADDER, Hardcore.SOFTCORE): None,
+            (Regions.ASIA, Ladder.NON_LADDER, Hardcore.HARDCORE): None,
+            (Regions.ASIA, Ladder.NON_LADDER, Hardcore.SOFTCORE): None,
+        }
+
+    def update(self):
+        progress_json = get_diablo_tracker()
+        updated_statuses = []
+
+        if not progress_json:
+            return None
+        else:
+            for entry in progress_json:
+                key = (int(entry["region"]), int(entry["ladder"]), int(entry["hc"]))
+                if not self.progress[key] == int(entry["progress"]):
+                    if self.progress[key]:
+                        updated_statuses.append(key)
+                    self.progress[key] = int(entry["progress"])
+
+        return updated_statuses
+
+    def text(self):
+        text = ""
+        for key, value in self.progress.items():
+            text += f"**[{value}/6]** {Regions.TEXT[key[0]]} {Ladder.TEXT[key[1]]} {Hardcore.TEXT[key[2]]}\n"
+        text += "> Data courtesy of diablo2.io"
+        return text
 
 
-@bot.command()
-async def uberdiablo(ctx):
-    response = get_diablo_tracker(
-        ladder=LadderFilter.NON_LADDER,
-        hardcore=HardcoreFilter.SOFTCORE,
-        sort_key=SortKey.REGION,
-    )
-    progresses = [p["progress"] for p in response.json()]
-    await ctx.send(
-        f"Diablo Clone Progress Tracker!\nAmericas: {progresses[0]}/6, Europe: {progresses[1]}/6, Asia: {progresses[2]}/6\nData courtesy of  diablo2.io"
-    )
+class DiscordClient(discord.Client):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.dclone_tracker = DCloneTracker()
+
+    async def on_ready(self):
+        self.report_status_update.start()
+
+    async def on_message(self, message):
+        if message.author == self.user:
+            return
+
+        if message.content.startswith("!uberdiablo"):
+            self.dclone_tracker.update()
+            await message.channel.send(self.dclone_tracker.text())
+
+    @tasks.loop(seconds=60)
+    async def report_status_update(self):
+        updated_statuses = self.dclone_tracker.update()
+
+        if not updated_statuses or not DISCORD_CHANNEL_ID:
+            return
+
+        message = ""
+        for key in updated_statuses:
+            progress = self.dclone_tracker.progress[key]
+            message += f"**[{progress}/6]** {Regions.TEXT[key[0]]} {Ladder.TEXT[key[1]]} {Hardcore.TEXT[key[2]]} DClone updated\n"
+
+        message += "> Data courtesy of diablo2.io"
+        channel = self.get_channel(DISCORD_CHANNEL_ID)
+        await channel.send(message)
+
+    @report_status_update.before_loop
+    async def setup(self):
+        await self.wait_until_ready()
 
 
 if __name__ == "__main__":
-    token = os.environ.get("TOKEN")
+    token = os.environ.get("DISCORD_TOKEN")
 
     if token:
-        bot.run(token)
+        client = DiscordClient(intents=discord.Intents.default())
+        client.run(token)
     else:
-        print("Please set the TOKEN environment variable!")
+        print("Please set the DISCORD_TOKEN environment variable!")
         exit(1)
